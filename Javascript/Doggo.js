2362b79bd5060170359fc898b3204a94a8553c26
mikkanen1 <91195560+mikkanen1@users.noreply.github.com>
2022-09-08 20:07:20 +0300

    A

---
+++ Javascript/Doggo.js
@@ -0,0 +1,34 @@
+const myAge = 25;
+
+let earlyYears = 2;
+
+earlyYears *= 7;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+const myAgeInDogYears = earlyYears + laterYears;
+
+const myName = "petteri" .toLowerCase()
+console.log(myName);
+
+
+var i = 5; var j = 10
+var k = 25; var l = Joe;
+
+if (i < j) { 
+ console.log("i");
+ if (j > k) console.log("k");
+ }
+else console.log("j");
+	var arr= [3, 5, 7];
+for (var i = 0; i <=3; ++i){
+console.log(arr[i]);
+}
+var arr = [3, 5, 7];
+for (var i = 0; i <=3; ++i){
+console.log(arr[i]);}
---
+++ Javascript/koodit.js
@@ -0,0 +1,4 @@
+alert("Anteeksi mutta taisin syödä kissan :D");
+console.log("Tämä on lokkimenoa!");
+document.write("Syötyäni jalopenoja päätin teetättää soutuveneelleni huulet!");
+document.write("<h2>Joskus keitän kahvia vodkaan!</h2>");
