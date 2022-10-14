20b3ea9acad77b3149317eda35be18078f6e0081
mikkanen1 <91195560+mikkanen1@users.noreply.github.com>
2022-10-14 14:13:02 +0300

    z

--- Javascript/teht1/main.js
+++ Javascript/teht1/main.js
@@ -1,9 +1,9 @@
-let addToDoButton  = document.getElementById('addToDo');
+let addToDoButton  = document.getElementById('lisää-nappi');
 let toDoContainer = document.getElementById('todoContainer');
 let inputField = document.getElementById('inputField')
 
 addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('P');
+    var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
